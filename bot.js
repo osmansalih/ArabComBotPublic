885fbb829b6ca01d0014dd74887e3388ba47655a
osmansalih <hpcplus2016@gmail.com>
2018-05-23 22:39:12 +0300

    bot.js

---
+++ bot.js
@@ -0,0 +1,12 @@
+const Discord = reqiure('discord.js');
+const client = New Discord.Client();
+client.login('Put Your Bot Token Here');
+const prefix = ('Your Bot Prefix Example : +');
+//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\
+console.log('==========================================');
+console.log(' = Ready With' + client.users.size + ' Users ! ');
+console.log(' = Ready On' + client.guilds.size + ' Guilds !');
+console.log(' = Ready In' + client.channels.size + ' Channels !');
+console.log(' = Prefix : ' + prefix);
+console.log('==========================================');
+/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\/\/\/\
